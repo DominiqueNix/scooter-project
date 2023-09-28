2a2a608a0cc7a951da88879d1b7250863edfc7a8
DominiqueNix <dominiquenix@google.com>
2023-09-28 11:31:20 -0500

    created rent and dock method tests

--- tests/Scooter.test.js
+++ tests/Scooter.test.js
@@ -2,6 +2,11 @@ const Scooter = require('../src/Scooter')
 const User = require('../src/User')
 
 
+let person = new User('mike', 'password123', 46)
+
+let scooter1 = new Scooter('abc123')
+
+let rent
 //typeof scooter === object
 describe('scooter object', () => {
   test('Scooter class should create Scooter instance', () => {
@@ -15,10 +20,44 @@ describe('scooter methods', () => {
   // tests here!
 
   //rent method
+test('rent method should see if charge is over 20 and not broken, then set the user to the user passed in and set station to null', ()=>{
 
+  scooter1.rent(person);
+
+  let receieved = {
+    'user': scooter1.user,
+    'station': scooter1.station
+  }
+
+  let expected = {
+    'user': person, 
+    'station': null
+  }
+
+  expect(receieved).toEqual(expected)
+  
+})
 
   //dock method
+  test("dock method should set the station the dock site and set the user to null", ()=> {
 
+    scooter1.rent(person);
+    scooter1.dock('hij456');
+
+    let receieved = {
+      "user": scooter1.user, 
+      'station': scooter1.station
+    }
+
+    let expected = {
+      "user": null, 
+      'station': 'hij456'
+    }
+
+    expect(receieved).toEqual(expected)
+    
+    
+  })
   //requestRepair method
 
   //charge method
