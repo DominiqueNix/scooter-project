be57d00bc1032c7c2a3bce4270d9cf0da280b9f4
DominiqueNix <dominiquenix@google.com>
2023-09-29 14:14:56 -0500

    created scooter class

--- src/ScooterApp.js
+++ src/ScooterApp.js
@@ -3,6 +3,92 @@ const Scooter = require('./Scooter')
 
 class ScooterApp {
   // ScooterApp code here
+  constructor(){
+    this.stations = {
+      'abc123' : [], 
+      'jk456' : [], 
+      'lmn789': []
+    }
+
+    this.registeredUsers = {}
+  }
+
+  registeredUser(username, password, age){
+      if(!this.registeredUsers[username] && age >= 18){
+          let user = new User(username, password, age)
+          this.registeredUsers[username] = user;
+          return user
+      } else {
+        throw new Error("already registered or too young to register")
+      }
+  }
+
+  loginUser(username, password){
+
+    if(this.registeredUsers[username]) {
+      this.registeredUsers[username].login(password)
+      console.log("user has been logged in")
+    } else {
+      throw new Error("Username or password is incorrect")
+    }
+    
+  }
+
+  logoutUser(username){
+    if(this.registeredUsers[username]) {
+      this.registeredUsers[username].logout()
+      console.log("user is logged out")
+    } else {
+      throw new Error("no such user is logged in")
+    }
+
+  }
+
+  createScooter(station){
+    if(this.stations[station]){
+      let scooter = new Scooter(station);
+      this.stations[station].push(scooter)
+      console.log('created new scooter')
+    } else {
+      throw new Error('no such station')
+    }
+  }
+
+  dockScooter(scooter, station){
+    if(scooter.station == null && this.stations[station]){
+      scooter.dock(station)
+      this.stations[station].push(scooter)
+
+    } else if(!this.stations[station]){
+      throw new Error("no such station")
+    } else if(scooter.staion !== null){
+      throw new Error("scooter is already taken")
+    }
+  }
+
+  rentScooter(scooter, user){
+    if(scooter.station !== null) {
+
+      for(const aStation in this.stations) {
+        for(let i = 0; i < this.stations[aStation].length; i++){
+          if(this.stations[aStation][i] == scooter) {
+            this.stations[aStation].splice(i, 1);
+          }
+        } 
+      }
+      scooter.rent(user)
+      console.log("scooter is rented")
+    } else {
+      throw new Error("scooter already rented")
+    }
+  }
+  print(){
+    console.log(this.registeredUsers);
+    console.log(this.stations);
+    console.log(this.stations['abc123'].length);
+    console.log(this.stations['jk456'].length);
+    console.log(this.stations['lmn789'].length);
+  }
  
 }
 
